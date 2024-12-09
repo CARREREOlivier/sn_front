d4ba48899717fe78631a9b4c9ee3433635106b94
CARREREOlivier <carrereolivier@yahoo.fr>
2024-12-09 14:13:58 +0100

    modified: home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NewsService} from '../news.service';
 import {DatePipe, NgForOf, SlicePipe} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {RecitsService} from '../recits.service';
 
 
 
@@ -13,6 +14,15 @@ interface News {
   creation_date: string;
 
 }
+
+interface Recit {
+  recit_id: number;
+  title: string;
+  description: string;
+  author : string;
+  creation_date: string;
+}
+
 @Component({
   imports: [
     DatePipe,
@@ -24,16 +34,20 @@ interface News {
   standalone: true,
   styleUrl: './home.component.css',
   templateUrl: './home.component.html',
-  providers: [NewsService],
+  providers: [NewsService, RecitsService, RecitsService],
 })
 
 export class HomeComponent  implements OnInit {
   latestNews: News | null = null;
+  recits: Recit[] = [];
 
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService, private recitsService: RecitsService) {}
   ngOnInit(): void {
     this.newsService.getLatestNews().subscribe((news) => {
       this.latestNews = news;
+    });
+    this.recitsService.getRecits().subscribe((data) => {
+      this.recits = data;
     });
   }
 
